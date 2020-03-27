fac5de8d95e2350b688d5c3e52bcd4ac4d14ff0b
Tito Alvarez <augustoalvarez@gmail.com>
2020-03-26 19:03:11 -0600

    Inclusión opcional del registro de personal en generación de token

--- src/client.php
+++ src/client.php
@@ -8,7 +8,6 @@ use Exception;
  * API Client para el Webservice de DEPPA - Evaluación Docente
  * REQUERIMIENTOS: php-curl curl
  */
-
 class APIClient
 {
     private $url_base;
@@ -24,17 +23,17 @@ class APIClient
     /**
      * Configuración inicial del API Client para la evaluación docente
      *
-     * @param string  $url_base      La url base sobre la cual se encuentran los servicios Rest
-     * @param string  $usuario       Nombre de usuario provisto a la unidad académica para integrarse al sistema de evaluación
+     * @param string $url_base La url base sobre la cual se encuentran los servicios Rest
+     * @param string $usuario Nombre de usuario provisto a la unidad académica para integrarse al sistema de evaluación
      *                             del personal.
-     * @param string  $passwd      Contraseña del usuario provisto a la unidad académica para integrarse al sistema de evaluación
+     * @param string $passwd Contraseña del usuario provisto a la unidad académica para integrarse al sistema de evaluación
      *                             del personal.
-     * @param int   $unidad   Código que identifica la unidad académica que está realizando la
+     * @param int $unidad Código que identifica la unidad académica que está realizando la
      *                             consulta.
-     * @param array $config          Configuración general para el API Client y la configuración de las peticiones por CURL (no usado
+     * @param array $config Configuración general para el API Client y la configuración de las peticiones por CURL (no usado
      *                             actualmente).
      */
-    function __construct($url_base, $usuario, $passwd, $unidad, $config)
+    public function __construct($url_base, $usuario, $passwd, $unidad, $config)
     {
         $this->url_base = $url_base;
         $this->usuario = $usuario;
@@ -51,12 +50,19 @@ class APIClient
      * @param   $data
      * @param   $config
      *
-     * @return array Retorna un array con el formato ["error" => true/false, "data" => respuesta_del_API, "exception" => stackTrace_del_error, descripcion => descripcion_del_error]
+     * @return array Retorna un array con el formato ['error' => true/false, 'data' => respuesta_del_API, 'exception' => stackTrace_del_error, descripcion => descripcion_del_error]
      */
     private function makeRequest($url_base, $apiMethod, $data, $config)
     {
-        $dataSend = array_merge($data, ["usuario" => $this->usuario, "password" => $this->passwd, "codigo_unidad" => $this->unidad]);
-        var_dump("Array enviado como parametros de la petición: ", $dataSend);
+        $dataSend = array_merge(
+            $data,
+            [
+                'usuario' => $this->usuario,
+                'password' => $this->passwd,
+                'codigo_unidad' => $this->unidad
+            ]
+        );
+        var_dump('Array enviado como parametros de la petición: ', $dataSend);
         try {
             $ch = curl_init();
             curl_setopt($ch, CURLOPT_URL, $url_base . $apiMethod);
@@ -67,15 +73,31 @@ class APIClient
             try {
                 $dataArray = json_decode($response, true);
                 if (!empty($dataArray)) {
-                    return ["error" => false, "data" => $dataArray];
+                    return ['error' => false, 'data' => $dataArray];
                 } else {
-                    return ["error" => true, "response" => $response, "descripcion" => "La respuesta no se encuentra bien formada.", "errorCode" => static::ERROR_BAD_REQUEST, 'responseCode' => curl_getinfo($ch, CURLINFO_HTTP_CODE)];
+                    return [
+                        'error' => true,
+                        'response' => $response,
+                        'descripcion' => 'La respuesta no se encuentra bien formada.',
+                        'errorCode' => static::ERROR_BAD_REQUEST,
+                        'responseCode' => curl_getinfo($ch, CURLINFO_HTTP_CODE)
+                    ];
                 }
             } catch (Exception $exc) {
-                return ["error" => true, "exception" => $exc->getTraceAsString(), "descripcion" => "Ha sucedido un error al intentar convertir la respuesta a un Array.", "errorCode" => static::ERROR_BAD_RESPONSE];
+                return [
+                    'error' => true,
+                    'exception' => $exc->getTraceAsString(),
+                    'descripcion' => 'Ha sucedido un error al intentar convertir la respuesta a un Array.',
+                    'errorCode' => static::ERROR_BAD_RESPONSE
+                ];
             }
         } catch (Exception $exc) {
-            return ["error" => true, "exception" => $exc->getTraceAsString(), "descripcion" => "Ha sucedido un error al intentar realizar la petición.", "errorCode" => static::ERROR_REQUEST];
+            return [
+                'error' => true,
+                'exception' => $exc->getTraceAsString(),
+                'descripcion' => 'Ha sucedido un error al intentar realizar la petición.',
+                'errorCode' => static::ERROR_REQUEST
+            ];
         }
     }
 
@@ -89,43 +111,56 @@ class APIClient
     public function verificarPeriodoDeEvaluacion($fecha)
     {
         $data = [
-            "fecha" => $fecha,
+            'fecha' => $fecha,
         ];
-        return $this->makeRequest($this->url_base, "/PeriodoEvaluacion", $data, $this->config);
+        return $this->makeRequest($this->url_base, '/PeriodoEvaluacion', $data, $this->config);
     }
 
     /**
      * Reservar un ticket para contestar la evaluación (encuesta) y enviar la información al componente que lo solicita para mostrarlo al evaluador.
      *
-     * @param int  $anio           Año al que corresponde el token
+     * @param int $anio Año al que corresponde el token
      *                             de evaluación.
      * @param string $codigo_periodo Periodo al que corresponde el token de evaluación.
-     * @param string $codigo_curso   Codigo de curso al que se desea evaluar en la nomenclatura de la unidad.
-     * @param string $seccion        Sección de curso al que se desea evaluar en la nomenclatura de la
+     * @param string $codigo_curso Codigo de curso al que se desea evaluar en la nomenclatura de la unidad.
+     * @param string $seccion Sección de curso al que se desea evaluar en la nomenclatura de la
      *                             unidad.
-     * @param string $token          Identificador del evaluador al que se genera el token. (Función Hash interna, independiente de la plataforma de
+     * @param string $token Identificador del evaluador al que se genera el token. (Función Hash interna, independiente de la plataforma de
      *                             encuestas)
-     * @param string $numero_boleta  Boleta con la que se desea evaluar al responsable del curso..
+     * @param string $numero_boleta Boleta con la que se desea evaluar al responsable del curso.
+     * @param string $registro_personal (Opcional) Registro de personal del docente a evaluar - importante cuando la sección es compartida por varios docentes
      *
      * @return array
      */
-    public function generarEnlaceEvaluacionEnLinea($anio, $codigo_periodo, $codigo_curso, $seccion, $token, $numero_boleta)
-    {
+    public function generarEnlaceEvaluacionEnLinea(
+        $anio,
+        $codigo_periodo,
+        $codigo_curso,
+        $seccion,
+        $token,
+        $numero_boleta,
+        $registro_personal = ''
+    ) {
         $data = [
-            "anio" => $anio,
-            "codigo_periodo" => $codigo_periodo,
-            "codigo_curso" => $codigo_curso,
-            "seccion" => $seccion,
-            "token" => $token,
-            "numero_boleta" => $numero_boleta,
+            'anio' => $anio,
+            'codigo_periodo' => $codigo_periodo,
+            'codigo_curso' => $codigo_curso,
+            'seccion' => $seccion,
+            'token' => $token,
+            'numero_boleta' => $numero_boleta,
         ];
-        return $this->makeRequest($this->url_base, "/EnlaceEvaluacionEnLinea", $data, $this->config);
+
+        if ($registro_personal !== '') {
+            $data['registro_personal'] = $registro_personal;
+        }
+
+        return $this->makeRequest($this->url_base, '/EnlaceEvaluacionEnLinea', $data, $this->config);
     }
 
     /**
      * Reservar un ticket para contestar la evaluación (encuesta) y enviar la información al componente que lo solicita para mostrarlo al evaluador.
      *
-     * @param int $sid   Identificador de la encuesta de LimeSurvey al que corresponde el token consultado.
+     * @param int $sid Identificador de la encuesta de LimeSurvey al que corresponde el token consultado.
      * @param string $token Identificador alfanumérico de la encuesta a consultar.
      *
      * @return array
@@ -133,9 +168,9 @@ class APIClient
     public function verificarEstadoEvaluacion($sid, $token)
     {
         $data = [
-            "sid" => $sid,
-            "token" => $token,
+            'sid' => $sid,
+            'token' => $token,
         ];
-        return $this->makeRequest($this->url_base, "/EstadoEvaluacion", $data, $this->config);
+        return $this->makeRequest($this->url_base, '/EstadoEvaluacion', $data, $this->config);
     }
 }
